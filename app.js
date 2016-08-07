8333ca2f5d34ee0958cdce54544c1d907b0ed2f3
sns2009 <sns.nazar@gmail.com>
2016-08-07 20:10:00 +0300

    no select

--- app.js
+++ app.js
@@ -1,61 +1,26 @@
-var contHeight = $("#cards").height();
+var contHeight = $('#cards').height();
 var boxSide;
 var topMargin = ($(window).height() - contHeight) / 2;
 var boxNumber;
 
 
 //calculating margin from top
-$("#cards").css({
-    "margin": topMargin + "px auto 0px"
+$('#cards').css({
+    'margin': topMargin + 'px auto 0px'
 });
 
 
 //initial filling with boxes
-boxNumber = $("#cards select option:selected").text();
-boxSide = 600 / Math.sqrt(boxNumber);
-for (var i = 1; i <= boxNumber; i++) {
-    $("#cards").append("<div class ='box'>" + i + "</div>");
+boxNumber = 16;
+boxSide = contHeight / Math.sqrt(boxNumber);
+for (var i = 0; i < boxNumber; i++) {
+    $('#cards').append('<div class ="box" data-item="' + i + '">' + i + '</div>');
 }
-$(".box").css({
-    "display": "block",
-    "height": boxSide + "px",
-    "width": boxSide + "px",
-    "border": "1px solid black",
-    "float": "left",
-    "box-sizing": "border-box"
+$('.box').css({
+
+    'height': boxSide + 'px',
+    'width': boxSide + 'px'
+    
 });
 
 
-//filling based on select
-$("#cards select").change(function () {
-    $(".box").remove();
-    console.log("event is happen");
-    boxNumber = $("#cards select option:selected").text();
-    switch (+boxNumber) {
-    case 9:
-        boxSide = 600 / Math.sqrt(boxNumber);
-        break;
-    case 16:
-        boxSide = 600 / Math.sqrt(boxNumber);
-        break;
-    case 25:
-        boxSide = 600 / Math.sqrt(boxNumber);
-        break;
-    default:
-        console.log("mistake happened");
-    }
-
-    for (var i = 1; i <= boxNumber; i++) {
-        $("#cards").append("<div class ='box'>" + i + "</div>");
-    }
-
-    $(".box").css({
-        "display": "block",
-        "height": boxSide + "px",
-        "width": boxSide + "px",
-        "border": "1px solid black",
-        "float": "left",
-        "box-sizing": "border-box"
-    });
-
-});
