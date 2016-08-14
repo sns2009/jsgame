dcbd4703d24ffdb09a3fa3dd17239599ec8e71fe
sns2009 <sns.nazar@gmail.com>
2016-08-14 21:31:30 +0300

    ramdom values

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ var contHeight = $('#cards').height();
 var boxSide;
 var topMargin = ($(window).height() - contHeight) / 2;
 var boxNumber;
+var imgArr = ['images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg','images/5.jpg','images/6.jpg','images/7.jpg','images/8.jpg'];
+var boxValue = [];
+var userOpened = [];
+var currentItem;
 
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 //calculating margin from top
 $('#cards').css({
@@ -23,4 +30,39 @@ $('.box').css({
     
 });
 
+//creating values for boxes
 
+function onlyTwoNotUnique(box){
+    var j = 0;
+    for(var k = 0; k < boxNumber; k++){
+        if(boxValue[box] === boxValue[k]){
+            j++;
+        }
+    }
+    if(j>2){
+        boxValue[i] = getRandomInt(0,8);
+        onlyTwoNotUnique(box);
+    }   
+}
+
+for(i = 0; i < boxNumber; i++){
+    boxValue[i] = getRandomInt(0,8);
+    if( !((i-1) <= 0) ){
+        onlyTwoNotUnique(i);   
+    }
+}
+
+$('.box').on('click',function(){
+    currentItem = $(this).data('item');
+    console.log(currentItem);
+    if(userOpened[currentItem] != true){
+        $(this).css({
+            'background':'url('+imgArr[boxValue[+currentItem]]+');'
+        });
+    }
+
+    
+    userClicked = true;
+});
+
+console.log(boxValue);
