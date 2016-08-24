99182ebcc36ae26a2e442f4be95a71d96b183cc0
sns2009 <sns.nazar@gmail.com>
2016-08-24 11:29:07 +0300

    full game logic, I guess:)

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var boxNumber;
 var imgArr = ['images/1.jpg','images/2.jpg','images/3.jpg','images/4.jpg','images/5.jpg','images/6.jpg','images/7.jpg','images/8.jpg'];
 var boxValue = [];
 var userOpened = [];
-var currentItem;
+var currentItem,previousItem;
+var newProperty = {};
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -21,7 +22,8 @@ $('#cards').css({
 boxNumber = 16;
 boxSide = contHeight / Math.sqrt(boxNumber);
 for (var i = 0; i < boxNumber; i++) {
-    $('#cards').append('<div class ="box" data-item="' + i + '">' + i + '</div>');
+    $('#cards').append('<div class ="box" data-item="' + i + '"></div>');
+    userOpened[i] = false;
 }
 $('.box').css({
 
@@ -45,6 +47,17 @@ function onlyTwoNotUnique(box){
     }   
 }
 
+function openItem(context,item){
+     $(context).css('background-image','url(' + imgArr[boxValue[item]] + ')');
+}
+function closeItems(currentItem,previousItem){
+    $('.box[data-item="'+currentItem+'"]').css('background-image','none');
+    $('.box[data-item="'+previousItem+'"]').css('background-image','none');
+    userOpened[currentItem] = false;
+    userOpened[previousItem] = false;
+    previousItem = undefined;
+    
+}
 for(i = 0; i < boxNumber; i++){
     boxValue[i] = getRandomInt(0,8);
     if( !((i-1) <= 0) ){
@@ -54,15 +67,47 @@ for(i = 0; i < boxNumber; i++){
 
 $('.box').on('click',function(){
     currentItem = $(this).data('item');
-    console.log(currentItem);
-    if(userOpened[currentItem] != true){
-        $(this).css({
-            'background':'url('+imgArr[boxValue[+currentItem]]+');'
-        });
-    }
-
     
-    userClicked = true;
+    
+    if(userOpened[currentItem] != true){
+        openItem(this,currentItem);
+        userOpened[currentItem] = true;
+        if(typeof previousItem != "undefined" && boxValue[currentItem] != boxValue[previousItem]){
+            closeItems(currentItem,previousItem);
+        }
+        if(boxValue[previousItem] == boxValue[currentItem]){
+            previousItem = undefined;
+        }else if(typeof previousItem != "undefined" && boxValue[previousItem] != boxValue[currentItem]){
+            previousItem = undefined;   
+        }else{
+            previousItem = currentItem;
+        }
+        
+        
+        
+    }
+    j = 0;
+    for(i = 0; i< (userOpened.length); i++){
+        if(userOpened[i] == true){
+            j++;
+            if( j == 16 ){
+                        setTimeout(function(){
+                            if(confirm('You\'ve played like a BOSS! One more time?)')){
+                            location.reload(true);
+                        }
+                        },200);
+                        
+                    }
+        }   
+    }
+    console.log('currentItem ' + currentItem);
+    console.log('previousItem ' + previousItem);
+    console.log(userOpened);
+    console.log(boxValue);
 });
 
+
 console.log(boxValue);
+
+
+
